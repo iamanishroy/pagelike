284710724f90f1e183dbf88a8cc0d0b0ceac9c3d
Anish Roy <6275anishroy@gmail.com>
2022-06-26 16:57:26 +0530

    added docs

---
+++ docs/script.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('code')[0].innerText = `<script defer src="https://unpkg.com/pagelike-plugin@1.0.2/dist/js/index.js"></script>`;
+document.getElementsByTagName('code')[1].innerText = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pagelike-plugin@1.0.2/dist/css/styles.css" />`
