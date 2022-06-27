5bf24bc4404789b696e53948a39ca315d08e0ee6
Anish Roy <6275anishroy@gmail.com>
2022-06-27 18:19:12 +0530

    minor fix

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 document.getElementsByTagName(
   "code"
-)[0].innerText = `<script defer src="https://unpkg.com/pagelike-plugin@1.2.0/dist/js/index.js"></script>`;
+)[0].innerText = `<script defer src="https://unpkg.com/pagelike-plugin@1.2.1/dist/js/index.js"></script>`;
 document.getElementsByTagName(
   "code"
-)[1].innerText = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pagelike-plugin@1.2.0/dist/css/styles.css" />`;
+)[1].innerText = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pagelike-plugin@1.2.1/dist/css/styles.css" />`;
